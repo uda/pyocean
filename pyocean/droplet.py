b322a0260afcb4a66d3fd908b2e9f02ebc56dd55
Nash <nash@bsd-noobz.com>
2014-09-20 15:57:48 +0700

    remove unnecessary parameter name

--- pyocean/droplet.py
+++ pyocean/droplet.py
@@ -30,7 +30,7 @@ class Droplet(Resource):
 
     def password_reset(self):
         """Password reset current droplet. New password will be emailed to you."""
-        return self.__do_action(type_='password_reset')
+        return self.__do_action('password_reset')
 
     def resize(self, size):
         pass
@@ -49,19 +49,19 @@ class Droplet(Resource):
 
     def enable_ipv6(self):
         """Enable IPv6 for current droplet."""
-        return self.__do_action(type_='enable_ipv6')
+        return self.__do_action('enable_ipv6')
 
     def disable_backups(self):
         """Disable backups for current droplet."""
-        return self.__do_action(type_='disable_backups')
+        return self.__do_action('disable_backups')
 
     def disable_backups(self):
         """Enable private networking for current droplet."""
-        return self.__do_action(type_='enable_private_networking')
+        return self.__do_action('enable_private_networking')
 
     def snapshot(self, name):
         """Snapshot current droplet."""
-        return self.__do_action(type_="snapshot", name=name)
+        return self.__do_action("snapshot", name=name)
 
     def __do_action(self, type_, name=None):
         """Perform action on droplet."""
